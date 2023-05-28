a7d49cadb7e326e0ac1fd9b0eeb92d30e8d591cd
Osama Jandali <osamajandaly@gmail.com>
2023-05-28 21:30:08 +0300

    Add loading to search

--- src/components/InsightsCatalog/index.tsx
+++ src/components/InsightsCatalog/index.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, PoweredBy, Hits, RefinementList } from 'react-instantsearch-hooks-web';
-
 import Card from "../Card";
 import Styles from "./styles.module.css";
-import CardStyles from "../Card/styles.module.css";
 
 import type { Insight } from "../types";
 
@@ -19,11 +16,35 @@ const InsightsCatalog = () => {
       <InstantSearch searchClient={searchClient} indexName="Insights - SoGS">
         <SearchBox
           placeholder="Search insights..."
+          classNames={
+            {
+              form: 'relative',
+              loadingIndicator: 'absolute right-16 top-7 ',
+            }
+          }
           resetIconComponent={() => (
             <></>
           )}
           loadingIconComponent={() => (
-            <p>Loading...</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="text-secondary" viewBox="0 0 38 38">
+              <defs>
+                <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+                  <stop stopColor="currentColor" stopOpacity="0" offset="0%" />
+                  <stop stopColor="currentColor" stopOpacity=".631" offset="63.146%" />
+                  <stop stopColor="currentColor" offset="100%" />
+                </linearGradient>
+              </defs>
+              <g fill="none" fillRule="evenodd">
+                <g transform="translate(1 1)">
+                  <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" strokeWidth="2">
+                    <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite" />
+                  </path>
+                  <circle fill="currentColor" cx="36" cy="18" r="1">
+                    <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite" />
+                  </circle>
+                </g>
+              </g>
+            </svg>
           )}
           submitIconComponent={() => (
             <svg
@@ -48,6 +69,7 @@ const InsightsCatalog = () => {
           <RefinementList
             attribute="tags"
             sortBy={['name:asc']}
+            className='h-[22px]'
             classNames={{
               list: 'flex flex-wrap gap-2',
               checkbox: 'hidden',

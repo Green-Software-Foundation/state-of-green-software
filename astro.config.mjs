104dd9de73c6d847effa6e8378ce429fe41927e4
Osama Jandali <osamajandaly@gmail.com>
2023-05-31 13:19:05 +0300

    Fixing little things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import rehypeExternalLinks from "rehype-external-links"
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -10,6 +10,14 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stateof.greensoftware.foundation',
+  markdown:{
+    rehypePlugins: [
+      [rehypeExternalLinks, {
+        target: '_blank',
+        rel: ['noopener', 'noreferrer']
+      }],
+    ]
+  },
   integrations: [react(), tailwind({
     config: {
       applyBaseStyles: false
